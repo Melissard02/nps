0b6d963691978c8579bec955897e8fc96f32126f
Melissard02 <melissa.rd1258@gmail.com>
2026-01-08 13:09:38 -0600

    JS updated

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,18 @@
 import { getParkData } from "./parkService.mjs";
 
 const parkData = getParkData();
+
+const disclaimer = document.querySelector(".disclaimer > a");
+disclaimer.href = parkData.url;
+disclaimer.innerHTML = parkData.fullName;
+
+const parkName = document.querySelector(".hero-banner__title")
+parkName.href = parkData.url;
+parkName.innerHTML = parkData.name;
+
+
+const states = document.querySelector(".states")
+states.innerHTML = parkData.states;
+
+const designation = document.querySelector(".designation")
+designation = parkData.designation
