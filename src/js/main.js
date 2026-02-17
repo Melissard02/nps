1b0b3aefa6e48516cf7098792d058335a8441b5d
Melissard02 <melissa.rd1258@gmail.com>
2026-02-17 15:21:18 -0600

    Part 5 Done

--- src/js/main.js
+++ src/js/main.js
@@ -17,6 +17,28 @@ function setParkInfoLinks(data) {
 
   infoEl.insertAdjacentHTML("afterbegin", html.join(""));
 }
+function enableNavigation() {
+  // use a querySelector to get the menu buttons
+    const menuButton = document.querySelector("#global-nav-toggle");
+  // when the main menu button is clicked:
+  menuButton.addEventListener("click", (ev) => {
+    let target = ev.target;
+    // toggle the show class on the global-nav
+    document.querySelector(".global-nav").classList.toggle("show");
+    // check to see if target is the button or something inside the button
+    if (target.tagName != "BUTTON") {
+      target = target.closest("button");
+    }
+    // check to see if we just opened or closed the menu
+    if (document.querySelector(".global-nav").classList.contains("show")) {
+      // if we opened it then set the aria-expanded attribute on the button to true
+      target.setAttribute("aria-expanded", true);
+    } else {
+      // if we closed it then set the aria-expanded attribute on the button to false
+      target.setAttribute("aria-expnded", false);
+    }
+  });
+}
 
 async function init() {
   const parkData = await getParkData();
@@ -24,6 +46,7 @@ async function init() {
   setHeaderFooter(parkData);
   setParkIntro(parkData);
   setParkInfoLinks(links);
+  enableNavigation();
 }
 
 init();
